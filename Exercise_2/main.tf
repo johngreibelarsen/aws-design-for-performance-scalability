7d452926c476e2998c9c9c3ab17d4ea3c1a17b2c
John Greibe Larsen <johnglarsen@hotmail.co.uk>
2021-10-29 10:19:58 +0100

    Serverless exercise

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -21,3 +21,25 @@ resource "aws_subnet" "udacity-subnet-public-2" {
     }
 }
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = file("iam/lambda_policy.json")
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = file("iam/lambda_assume_role_policy.json")
+}
+
+resource "aws_lambda_function" "udacity_lambda" {
+  function_name = "greet_lambda"
+  s3_bucket     = "udacity-lambda-jgl-bucket"
+  s3_key        = "greet_lambda.zip"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "greet_lambda.lambda_handler"
+  runtime       = "python3.8"
+  tags = {
+      Name = "udacity_lambda"
+  }
+}
--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,18 @@
+output "vpc_id" {
+  value = aws_vpc.udacity-vpc-2.id
+}
+
+output "subnet_private_2" {
+  value = aws_subnet.udacity-subnet-public-2.id
+}
+
 # TODO: Define the output variable for the lambda function.
+output "lambda_function_name" {
+  description = "Name of the Lambda function."
+  value = aws_lambda_function.udacity_lambda.function_name
+}
+output "lambda_arn" {
+  description = "The Amazon Resource Name (ARN) identifying the Lambda function."
+  value       = aws_lambda_function.udacity_lambda.arn
+}
+
